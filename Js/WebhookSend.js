3b0073ba09ec6fe5b1287353c1cde592c9ab4a5d
Developers-fun <167205453+developers-fun@users.noreply.github.com>
2024-08-21 20:16:17 -0500

    1

--- Js/WebhookSend.js
+++ Js/WebhookSend.js
@@ -1,12 +1,15 @@
-var SentYet = false
-
 function message() {
+    let SentYet = false
     let name = document.getElementById("Input1").value;
     let message = document.getElementById("Input2").value;
   
     if (SentYet == false) {
       const webhook = "https://discord.com/api/webhooks/1275787515265028137/9lD2eThfQV6dcIuBvijt5EsmYRPAwVsotPpeO_4M-J5eAwgilvciOcAddLuOghw5CcHI";
-      const contents = `A user wanted the game: ${name}\nWith the url: ${message}<@937481339362508820>`;
+      const contents = `A user wanted the game: ${name}\nWith the url: ${message} <@937481339362508820>`;
+      const embed = discord.Embed(
+        title = 'A new request has been made.',
+        description =  `A user wanted ${name}/nWith the matching url: ${message}/n<@937481339362508820>`
+        )
       const request = new XMLHttpRequest();
       request.open("POST", webhook);
       request.setRequestHeader('Content-type', 'application/json');
