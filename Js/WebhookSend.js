d692ee6392347d2ac117316a942caa1f6c53509d
Developers-fun <167205453+developers-fun@users.noreply.github.com>
2024-08-21 19:58:40 -0500

    multi-webhook fixer

--- Js/WebhookSend.js
+++ Js/WebhookSend.js
@@ -1,14 +1,21 @@
+var SentYet = false
+
 function message() {
     let name = document.getElementById("Input1").value;
     let message = document.getElementById("Input2").value;
   
-    const webhook = "https://discord.com/api/webhooks/1275787515265028137/9lD2eThfQV6dcIuBvijt5EsmYRPAwVsotPpeO_4M-J5eAwgilvciOcAddLuOghw5CcHI";
-    const contents = `A user wanted the game: ${name}\nWith the url: ${message}<@937481339362508820>`;
-    const request = new XMLHttpRequest();
-    request.open("POST", webhook);
-    request.setRequestHeader('Content-type', 'application/json');
-    const params = {
-      content: contents
+    if (SentYet == false) {
+      const webhook = "https://discord.com/api/webhooks/1275787515265028137/9lD2eThfQV6dcIuBvijt5EsmYRPAwVsotPpeO_4M-J5eAwgilvciOcAddLuOghw5CcHI";
+      const contents = `A user wanted the game: ${name}\nWith the url: ${message}<@937481339362508820>`;
+      const request = new XMLHttpRequest();
+      request.open("POST", webhook);
+      request.setRequestHeader('Content-type', 'application/json');
+      const params = {
+        content: contents
+      }
+      request.send(JSON.stringify(params));
+      SentYet = true
+    } else {
+      console.log('Too many requests to webhooks')
     }
-    request.send(JSON.stringify(params));
   }
