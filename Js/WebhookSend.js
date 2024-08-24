b2d0becc842303a9cce14507ebc4bea8c5bfe533
Developers-fun <167205453+developers-fun@users.noreply.github.com>
2024-08-24 09:33:43 -0500

    Update WebhookSend.js

--- Js/WebhookSend.js
+++ Js/WebhookSend.js
@@ -1,9 +1,7 @@
 function message() {
-    let SentYet = false
     let name = document.getElementById("Input1").value;
     let message = document.getElementById("Input2").value;
   
-    if (SentYet == false) {
       const webhook = "https://discord.com/api/webhooks/1275787515265028137/9lD2eThfQV6dcIuBvijt5EsmYRPAwVsotPpeO_4M-J5eAwgilvciOcAddLuOghw5CcHI";
       const contents = `A user wanted the game: ${name}\nWith the url: ${message} <@937481339362508820>`;
       const request = new XMLHttpRequest();
@@ -14,7 +12,5 @@ function message() {
       }
       request.send(JSON.stringify(params));
       SentYet = true
-    } else {
-      console.log('Too many requests to webhooks')
-    }
+    
   }
