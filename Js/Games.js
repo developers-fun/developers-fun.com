4f6bee351ff83c2de1d9ce490b69a834d9bbd716
Developers-fun <167205453+developers-fun@users.noreply.github.com>
2024-08-29 13:57:40 -0500

    1

--- Js/Games.js
+++ Js/Games.js
@@ -242,7 +242,7 @@ var LocalGames = `{
           "name": "Tiny-Fishing",
           "creator": "Mad Buffer",
           "path": "/Games/Tiny-Fishing.html",
-          "image": "/Images/Images/Tiny-Fishing.png",
+          "image": "/Images/Images/Tiny-Fishing.jpeg",
           "Visible": true
       }
   ]
