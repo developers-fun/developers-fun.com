eeb392be230468e31ce5c843caab7896985e29c0
Developers-fun <167205453+developers-fun@users.noreply.github.com>
2024-11-13 16:17:55 +0000

    1

--- banana.js
+++ banana.js
@@ -1,16 +1,9 @@
+const { join, extname, basename } = require('path');
 const fs = require('fs');
-const path = require('path');
-const gamesDir = path.join(__dirname, 'G');
-if (!fs.existsSync(gamesDir)) {
-    fs.mkdirSync(gamesDir);
-}
 
-// Delete files with spaces in the G directory
-const files = fs.readdirSync(gamesDir);
-files.forEach(file => {
-    if (file.includes(' ')) {
-        const filePath = path.join(gamesDir, file);
-        fs.unlinkSync(filePath);
-        console.log(`Deleted file with spaces: ${file}`);
+for (const File of readdirSync(GameImgs)) {
+    const name = basename(oldFile, extension);
+    if (name.includes("-")) {
+        name.replaceAll("-", "")
     }
-});
+}
