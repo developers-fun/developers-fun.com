97d5ba2ccbdeef521d37ee23186ffb0b712b68bd
Xpectedlol <JOjooakpark@gmail.com>
2024-12-11 06:25:23 -0600

    name changes

--- banana.js
+++
@@ -1,9 +0,0 @@
-const { join, extname, basename } = require('path');
-const fs = require('fs');
-
-for (const File of readdirSync(GameImgs)) {
-    const name = basename(oldFile, extension);
-    if (name.includes("-")) {
-        name.replaceAll("-", "")
-    }
-}
